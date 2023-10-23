bfc08b36a71a7b37bf5378560ee4f65eb618de4b
xdKya <90745676+xdKya@users.noreply.github.com>
2023-10-23 18:09:46 -0300

    Update Home.js

--- screens/Home.js
+++ screens/Home.js
@@ -23,35 +23,7 @@ export default class HomeScreen extends Component {
         return (
             <View style={styles.container}>
                 <SafeAreaView style={styles.droidSafeArea} />
-                <ImageBackground source={require('../assets/bg.png')} style={styles.backgroundImage}>
-                    <View style={styles.titleBar}>
-                        <Text style={styles.titleText}>App Rastreador EEI</Text>
-                    </View>
-                    <TouchableOpacity style={styles.routeCard} onPress={() =>
-                        this.props.navigation.navigate("IssLocation")
-                    }>
-                        <Text style={styles.routeText}>Localização da EEI</Text>
-                        <Text style={styles.knowMore}>{"Saiba Mais --->"}</Text>
-                        <Text style={styles.bgDigit}>1</Text>
-                        <Image source={require("../assets/iss_icon.png")} style={styles.iconImage}></Image>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.routeCard} onPress={() =>
-                        this.props.navigation.navigate("Meteors")
-                    }>
-                        <Text style={styles.routeText}>Meteoros</Text>
-                        <Text style={styles.knowMore}>{"Saiba Mais --->"}</Text>
-                        <Text style={styles.bgDigit}>2</Text>
-                        <Image source={require("../assets/meteor_icon.png")} style={styles.iconImage}></Image>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={styles.routeCard} onPress={() =>
-                        this.props.navigation.navigate("Updates")
-                    }>
-                        <Text style={styles.routeText}>Atualizações</Text>
-                        <Text style={styles.knowMore}>{"Know More --->"}</Text>
-                        <Text style={styles.bgDigit}>3</Text>
-                        <Image source={require("../assets/rocket_icon.png")} style={styles.iconImage}></Image>
-                    </TouchableOpacity>
-                </ImageBackground>
+                
             </View>
         );
     }
@@ -67,44 +39,6 @@ const styles = StyleSheet.create({
     backgroundImage: {
         flex: 1,
         resizeMode: 'cover',
-    },
-    routeCard: {
-        flex: 0.25,
-        marginLeft: 50,
-        marginRight: 50,
-        marginTop: 50,
-        borderRadius: 30,
-        backgroundColor: 'white'
-    },
-    titleBar: {
-        flex: 0.15,
-        justifyContent: "center",
-        alignItems: "center"
-    },
-    titleText: {
-        fontSize: 40,
-        fontWeight: "bold",
-        color: "white"
-    },
-    routeText: {
-        fontSize: 35,
-        fontWeight: "bold",
-        color: "black",
-        marginTop: 75,
-        paddingLeft: 30
-    },
-    knowMore: {
-        paddingLeft: 30,
-        color: "red",
-        fontSize: 15
-    },
-    bgDigit: {
-        position: "absolute",
-        color: "rgba(183, 183, 183, 0.5)",
-        fontSize: 150,
-        right: 20,
-        bottom: -15,
-        zIndex: -1
     },
     iconImage: {
         position: "absolute",
